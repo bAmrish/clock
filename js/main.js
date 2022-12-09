9f6a2b3fc18b4661f5dd3c968f7249e9eb2df78e
Amrish Bharatiya <bamrish@gmail.com>
2022-12-10 03:36:19 +0530

    initial code for clock

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ const init = () => {
   const CLOCK_MARGIN = 50;
   const CENTER_X = CANVAS_WIDTH / 2;
   const CENTER_Y = CANVAS_WIDTH / 2;
-  const CLOCK_DIAMETER
-    = CANVAS_WIDTH - CLOCK_MARGIN * 2;
+  const CLOCK_DIAMETER = CANVAS_WIDTH - CLOCK_MARGIN * 2;
   const CLOCK_RADIUS = CLOCK_DIAMETER / 2;
   const NUMBER_MARGIN = 35;
   const draw = SVG().addTo('body').size(CANVAS_WIDTH, CANVAS_HEIGHT).id("canvas");
   draw.rect(CANVAS_WIDTH, CANVAS_WIDTH).fill('none').stroke('none')
+
   const clockFace = draw.circle(CLOCK_DIAMETER + 35)
     .center(CENTER_X, CENTER_Y)
     .dy(-5)
@@ -19,25 +19,37 @@ const init = () => {
     .fill('rgba(112,255,135,0.56)')
     .css({"filter": "drop-shadow(3px 3px 3px #3C2003FF)"});
 
+  const clockCenter = draw.circle(5)
+    .center(CENTER_X, CENTER_Y)
+    .fill('rgba(34,34,34,0.48)')
+    .css({"filter": "drop-shadow(3px 3px 3px #3C2003FF)"});
 
 
-  const minuteHand = draw.line(CENTER_X, CENTER_Y, CENTER_X, CLOCK_MARGIN + NUMBER_MARGIN).stroke({
-    width: 2,
-    color: "#000000"
-  }).css({"filter": "drop-shadow(3px 3px 3px #3C2003FF)"});
-  const hourHand = draw.line(CENTER_X, CENTER_Y, CENTER_X, CLOCK_MARGIN + NUMBER_MARGIN * 2).stroke({
-    width: 4,
-    color: "#000000"
-  }).css({"filter": "drop-shadow(3px 3px 3px #3C2003FF)"});
-  // draw.line(CANVAS_WIDTH / 2, CLOCK_MARGIN + 10, CANVAS_WIDTH/2, CLOCK_MARGIN + 20).stroke({width: 4, color: "red"})
+  const minuteHand = draw.line(CENTER_X, CENTER_Y, CENTER_X, CLOCK_MARGIN + NUMBER_MARGIN)
+    .stroke({
+      width: 2,
+      color: "#000000"
+    })
+    .css({"filter": "drop-shadow(3px 3px 3px #3C2003FF)"});
+
+  const hourHand = draw.line(CENTER_X, CENTER_Y, CENTER_X, CLOCK_MARGIN + NUMBER_MARGIN * 2)
+    .stroke({
+      width: 4,
+      color: "#000000"
+    })
+    .css({"filter": "drop-shadow(3px 3px 3px #3C2003FF)"});
+
 
   for (let i = 1; i <= 12; i++) {
     const angleInDeg = (360 / 12) * i;
-    // const angleInRad = ((angleInDeg) * Math.PI) / 180;
-    // draw.line(CENTER_X, CENTER_Y, CENTER_Y, CENTER_Y - NUMBER_MARGIN)
-    //   .stroke({width: 0, color: "red"})
-    //   .rotate(angleInDeg)
-    //   .dy(-1 * (CLOCK_RADIUS - NUMBER_MARGIN / 2 * Math.cos(angleInRad)))
+    const angleInRad = ((angleInDeg) * Math.PI) / 180;
+
+    const tick_length = 15;
+    draw.line(CENTER_X, CENTER_Y, CENTER_Y, CENTER_Y - tick_length)
+      .stroke({width: 0, color: "black"})
+      .rotate(angleInDeg)
+      .dy(-1 * (CLOCK_RADIUS - (tick_length / 2) * Math.cos(angleInRad)))
+      .dx((tick_length / 2) * (Math.sin(angleInRad)));
 
     const t = i < 10 ? "" + i : "" + i;
     draw.text(t)
@@ -55,6 +67,7 @@ const init = () => {
 
   const min = Math.floor(Math.random() * 12) * 5;
   const hour = 1 + Math.floor(Math.random() * 12 - 1);
+
   console.log(`${hour}:${min}`)
   minuteHand.rotate((360 * min) / 60, CENTER_X, CENTER_Y)
   hourHand.rotate(((360 * hour) / 12) + ((360 * min) / (12 * 60)), CENTER_X, CENTER_Y)
