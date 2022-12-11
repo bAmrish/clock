b9db0108544c58548974fe41881a6789f6b9eab5
Amrish Bharatiya <bamrish@gmail.com>
2022-12-11 19:51:06 +0530

    Initial code for app styling

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,19 +1,36 @@
 import {Clock} from "./clock";
 
+const toggleMenu = () => {
+  const toolbar = document.querySelector("#toolbar-items");
+  if(toolbar.classList.contains('hidden')){
+    toolbar.classList.remove('hidden')
+  } else {
+    toolbar.classList.add('hidden')
+  }
+}
 
 window.addEventListener('DOMContentLoaded', () => {
   const clockNode = document.getElementById("clock");
   console.log(clockNode)
   const c: Clock = new Clock(clockNode);
+
   const currentTimeRadio = document.getElementById('clock-type-current-time');
+  const learnRadio = document.getElementById('clock-type-learn');
   currentTimeRadio.addEventListener('click', () => {
     c.currentTime();
+    toggleMenu();
   });
-  const testRadio = document.getElementById('clock-type-test');
-  testRadio.addEventListener('click', () => {
+  learnRadio.addEventListener('click', () => {
     const min = Math.floor(Math.random() * 12) * 5;
     const hour = 1 + Math.floor(Math.random() * 12 - 1);
     c.showTime(hour, min).hideSeconds();
     console.log(`${hour}:${min}`)
+    toggleMenu();
   });
+
+  const menu = document.querySelector("#menu");
+
+  menu.addEventListener('click', () => {
+    toggleMenu();
+  })
 })
